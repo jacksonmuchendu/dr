a7c9b892edc4b9e9d39eba44bf4b647593c7474a
jacksonmuchendu <jacksonmuchendu2000@gmail.com>
2024-12-16 12:36:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function exploreMore() {
+    alert("Explore our resources to start your mental health journey!");
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for reaching out! We will get back to you soon.");
+});
